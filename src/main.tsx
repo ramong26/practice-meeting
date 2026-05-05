b6d370646eb0fca66369bfed143bce6b4bc49ef3
김수연 <rlatndus21049@gmail.com>
2026-05-05 17:44:50 +0900

    chore: main  - 누락된 파일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import {
-  RouterProvider,
-  // createMemoryHistory,
-  createRouter,
-} from '@tanstack/react-router';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from '@/routeTree.gen';
 
 // const router = createRouter({ routeTree, history: createMemoryHistory() });
